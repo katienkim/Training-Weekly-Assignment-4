2f5eb13c8020a8da5f31384b3b13baaf0aaa0eb0
katienkim <katie.kim@megazone.com>
2025-08-06 14:43:35 -0700

    finished list-s3-bucket script that lists out name, url, and encrypted status of each bucket

---
+++ list-s3-buckets.py
@@ -0,0 +1,17 @@
+import boto3
+
+bucketList = []
+s3 = boto3.resource('s3')
+clients3 = boto3.client('s3')
+
+for bucket in s3.buckets.all():
+    currbucket = {}
+    name = bucket.name
+    region = clients3.get_bucket_location(Bucket=name)['LocationConstraint']
+
+    currbucket["Name"] = name
+    currbucket["URL"] = f'http://{name}.s3-website.{region}.amazonaws.com'
+    currbucket["Encrypted"] = clients3.get_bucket_encryption(Bucket=name)['ServerSideEncryptionConfiguration']['Rules'][0]['BucketKeyEnabled']
+    bucketList.append(currbucket)
+
+print(bucketList)
